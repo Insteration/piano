d55e628b15152085f1e36323dbba7403aa2edea1
Artem Karmaz <inste@icloud.com>
2018-12-10 23:47:52 +0200

    Initial Commit

---
+++ piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  piano
+//
+//  Created by Artem Karmaz on 12/10/18.
+//  Copyright © 2018 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
