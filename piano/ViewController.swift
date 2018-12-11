7526645b73190817b2d05755910a56aa786ec31c
Artem Karmaz <inste@icloud.com>
2018-12-11 19:45:22 +0200

    added array of sounds, remove from protocols variables, added new tag

--- piano/ViewController.swift
+++ piano/ViewController.swift
@@ -8,45 +8,12 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
-    
     let piano = PianoSounds()
     
     @IBAction func pianoPlayButton(_ sender: UIButton) {
-        guard let button = sender as? UIButton else {
-            return
-        }
-        switch button.tag {
-        case 0:
-            piano.playPiano(piano.c1)
-        case 1:
-            piano.playPiano(piano.c1s)
-        case 2:
-            piano.playPiano(piano.d1)
-        case 3:
-            piano.playPiano(piano.d1s)
-        case 4:
-            piano.playPiano(piano.e1)
-        case 5:
-            piano.playPiano(piano.f1)
-        case 6:
-            piano.playPiano(piano.f1s)
-        case 7:
-            piano.playPiano(piano.g1)
-        case 8:
-            piano.playPiano(piano.g1s)
-        case 9:
-            piano.playPiano(piano.a1)
-        case 10:
-            piano.playPiano(piano.a1s)
-        case 11:
-            piano.playPiano(piano.b1)
-        case 12:
-            piano.playPiano(piano.c2)
-        default:
-            return
+        for _ in piano.sounds {
+            piano.playPiano(piano.sounds[sender.tag])
         }
     }
 }
--- piano/model.swift
+++ piano/model.swift
@@ -10,35 +10,11 @@ import Foundation
 import AVFoundation
 
 protocol Piano {
-    var c1: String {get}
-    var c1s: String {get}
-    var c2: String {get}
-    var d1: String {get}
-    var d1s: String {get}
-    var e1: String {get}
-    var f1: String {get}
-    var f1s: String {get}
-    var g1: String {get}
-    var g1s: String {get}
-    var a1: String {get}
-    var a1s: String {get}
-    var b1: String {get}
+func playPiano(_ sound: String)
 }
 
 class PianoSounds: Piano {
-    var c1: String = Bundle.main.path(forResource: "c1", ofType: "wav", inDirectory: "data/sounds")!
-    var c1s: String = Bundle.main.path(forResource: "c1s", ofType: "wav", inDirectory: "data/sounds")!
-    var c2: String = Bundle.main.path(forResource: "c2", ofType: "wav", inDirectory: "data/sounds")!
-    var d1: String = Bundle.main.path(forResource: "d1", ofType: "wav", inDirectory: "data/sounds")!
-    var d1s: String = Bundle.main.path(forResource: "d1s", ofType: "wav", inDirectory: "data/sounds")!
-    var e1: String = Bundle.main.path(forResource: "e1", ofType: "wav", inDirectory: "data/sounds")!
-    var f1: String = Bundle.main.path(forResource: "f1", ofType: "wav", inDirectory: "data/sounds")!
-    var f1s: String = Bundle.main.path(forResource: "f1s", ofType: "wav", inDirectory: "data/sounds")!
-    var g1: String = Bundle.main.path(forResource: "g1", ofType: "wav", inDirectory: "data/sounds")!
-    var g1s: String = Bundle.main.path(forResource: "g1s", ofType: "wav", inDirectory: "data/sounds")!
-    var a1: String = Bundle.main.path(forResource: "a1", ofType: "wav", inDirectory: "data/sounds")!
-    var a1s: String = Bundle.main.path(forResource: "b1", ofType: "wav", inDirectory: "data/sounds")!
-    let b1 = Bundle.main.path(forResource: "b1", ofType: "wav", inDirectory: "data/sounds")!
+    let sounds: [String] = [Bundle.main.path(forResource: "c1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "c1s", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "d1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "d1s", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "e1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "f1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "f1s", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "g1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "g1s", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "a1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "a1s", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "b1", ofType: "wav", inDirectory: "data/sounds")!, Bundle.main.path(forResource: "c2", ofType: "wav", inDirectory: "data/sounds")!]
     
     private var audioPlayer = AVAudioPlayer()
     
